a1a696f39642fe0e86dab613627bc62568e8cb26
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-02-13 21:14:22 +0530

    Time: 22 ms (79.68%) | Memory: 11.6 MB (36.53%) - LeetSync

---
+++ 1723-maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int result=INT_MIN;
+    int m;
+    void solve(int idx,int count,int n,vector<int>&resultant,vector<vector<int>>&requests){
+        if(idx>=m){
+            bool allzero=true;
+            for(int x:resultant){
+                if(x!=0){
+                    allzero=false;
+                    break;
+                }
+            }
+            if(allzero==true){
+                result=max(result,count);
+            }
+            return;
+        }
+        int from=requests[idx][0];
+        int to=requests[idx][1];
+
+        resultant[from]--;
+        resultant[to]++;
+        solve(idx+1,count+1,n,resultant,requests);
+        resultant[from]++;
+        resultant[to]--;
+        solve(idx+1,count,n,resultant,requests);
+
+    }
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        m=requests.size();
+        vector<int>resultant(n,0);
+        solve(0,0,n,resultant,requests);
+        return result;
+    }
+};
