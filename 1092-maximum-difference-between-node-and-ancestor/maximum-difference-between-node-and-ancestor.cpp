51af676cb70104ac8774cf84568bccf505a672b0
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-03-06 18:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (31.92%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -1,29 +1,48 @@
 
+// class Solution {
+// public:
+//     int maxdiff;
+
+//     void solvehelper(TreeNode * root,TreeNode * child){
+//         if(root==NULL || child==NULL) return ;
+
+//         maxdiff= max(maxdiff, abs(root->val - child->val));
+//         solvehelper(root,child->left);
+//         solvehelper(root,child->right);
+
+//     }
+
+//     void solve(TreeNode* root){
+//         if(root==NULL) return;
+
+//         solvehelper(root,root->left);
+//         solvehelper(root,root->right);
+
+//         solve(root->left);
+//         solve(root->right);
+//     }
+//     int maxAncestorDiff(TreeNode* root) {
+//         maxdiff=-1;
+//         solve(root);
+//         return maxdiff;
+//     }
+// };
 class Solution {
 public:
-    int maxdiff;
+    int findmax(TreeNode * root, int minv,int maxv){
+        if(!root){
+            return abs(minv-maxv);
 
-    void solvehelper(TreeNode * root,TreeNode * child){
-        if(root==NULL || child==NULL) return ;
+        }
+        minv=min(minv,root->val);
+        maxv=max(maxv,root->val);
+        int l=findmax(root->left,minv,maxv);
 
-        maxdiff= max(maxdiff, abs(root->val - child->val));
-        solvehelper(root,child->left);
-        solvehelper(root,child->right);
+        int r=findmax(root->right,minv,maxv);
 
-    }
-
-    void solve(TreeNode* root){
-        if(root==NULL) return;
-
-        solvehelper(root,root->left);
-        solvehelper(root,root->right);
-
-        solve(root->left);
-        solve(root->right);
+        return max(l,r);
     }
     int maxAncestorDiff(TreeNode* root) {
-        maxdiff=-1;
-        solve(root);
-        return maxdiff;
+        return findmax(root,root->val,root->val);
     }
 };
