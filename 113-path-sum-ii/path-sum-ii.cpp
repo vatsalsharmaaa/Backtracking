159be410dc7b30b09ffbc7c5b512819c1f65ca50
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-03-09 21:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (42.75%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -5,16 +5,12 @@ public:
 
     void solve(TreeNode* root, int target) {
         if(root == nullptr) return;
-
-        
-        curr.push_back(root->val);
-
-      
+    
+        curr.push_back(root->val); 
         if(root->left == nullptr && root->right == nullptr && target == root->val) {
             path.push_back(curr);
         }
-
-        
+   
         solve(root->left, target - root->val);
         solve(root->right, target - root->val);
         curr.pop_back();
