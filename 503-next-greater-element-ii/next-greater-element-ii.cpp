d7fdadcbf869d81ec1fa7f7cbd0633aea1d782ab
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-04-02 23:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (36.20%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,16 +1,38 @@
+// o(n*n) tc will give tle
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>res(n,-1);
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<2*n ;j++){
+//                 int id=j%n;
+//                 if(nums[id]>nums[i]){
+//                     res[i]=nums[id]; break;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+//optimal stack
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-        vector<int>res(n,-1);
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<2*n ;j++){
-                int id=j%n;
-                if(nums[id]>nums[i]){
-                    res[i]=nums[id]; break;
-                }
+        vector<int>nge(n);
+        stack<int>st;
+        for(int i=2*n-1;i>=0;i--){
+            int curr= nums[i%n];
+            while(!st.empty()&&st.top()<=curr){
+                st.pop();
             }
+            if(i<n){
+                nge[i]=st.empty()?-1:st.top();
+            }
+            st.push(curr);
         }
-        return res;
+        return nge;
     }
 };
