bbe9ec6237d6e4801ead99a90390184f25c31d1c
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-04-06 00:58:39 +0530

    Time: 6 ms (48.52%) | Memory: 11 MB (86.90%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string convert(string s, int num_rows) {
+        if (num_rows == 1) {
+            return s;
+        }
+
+        string ans;
+        int n = s.size();
+        int char_in_section = 2 * (num_rows - 1);
+
+        for (int curr_row = 0; curr_row < num_rows; curr_row++) {
+            int index = curr_row;
+
+            while (index < n) {
+                ans += s[index];
+
+                if (curr_row != 0 && curr_row != num_rows - 1) {
+                    int char_in_between = char_in_section - 2 * (curr_row);
+                    int second_index = index + char_in_between;
+
+                    if (second_index < n) {
+                        ans += s[second_index];
+                    }
+                }
+
+                index += char_in_section;
+            }
+        }
+
+        return ans;
+    }
+};
