d7b41282c2501b5e7d7f306d7c8801e6cd333fd4
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-05-13 17:08:36 +0530

    Time: 0 ms (100.00%) | Memory: 61.1 MB (27.59%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,22 +1,67 @@
+// o(n*n)
+// class Solution {
+// public:
+//     int longestNiceSubarray(vector<int>& nums) {
+//         int n=nums.size();
+//     int result= 0;
+//         for(int i=0;i<n;i++){
+//             int mask= 0;
+//             for(int j=i;j<n;j++){
+//                 if((mask & nums[j])!=0){
+//                     break;
+//                 }
+//                 else{
+//                     mask|=nums[j];
+                    
+//                 }
+//                 result= max(result,j-i+1);
+                
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+//sliding window o(n)
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
         int n=nums.size();
-    int result= 0;
-        for(int i=0;i<n;i++){
-            int mask= 0;
-            for(int j=i;j<n;j++){
-                if((mask & nums[j])!=0){
-                    break;
-                }
-                else{
-                    mask|=nums[j];
-                    
-                }
-                result= max(result,j-i+1);
-                
+        int i=0;
+        int j=0;
+        int result=0;
+        
+        int mask=0;
+
+        while(j<n){
+            while((mask & nums[j])!=0){
+                mask=( mask ^ nums[i]);
+                i++;
             }
+            result= max(result,j-i+1);
+            mask|= nums[j];
+            
+            j++;
         }
         return result;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
