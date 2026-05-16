65c9a728b4b10a1042952cb6abca857999deaf34
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-05-17 00:21:01 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (27.34%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,7 +1,66 @@
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int n=nums.size();
+//         int l=0;
+//         int r=n-1;
+//         while(l<r &&nums[l]==nums[l+1]){
+//             l++;
+//         }
+//          while(l<r&&nums[r]==nums[r-1]){
+//            r--;
+//         }
+//         int minidx=0;
+//         while(l<r){
+//             int mid= l+(r-l)/2;
+//             if(nums[mid]<nums[minidx]){
+//                 minidx=mid;
+//             }
+//             if(nums[mid]>nums[r]){
+               
+//                 l=mid+1;
+//             }
+//             else r=mid-1;
+//         }
+//         return nums[minidx];
+//     }
+// };
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[0];
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1;
+
+        while (l < r && nums[l] == nums[l + 1]) {
+            l++;
+        }
+
+        while (l < r && nums[r] == nums[r - 1]) {
+            r--;
+        }
+
+        int minidx = 0;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] < nums[minidx]) {
+                minidx = mid;
+            }
+
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            }
+            else {
+                r = mid;   // corrected from mid-1
+            }
+        }
+
+        if (nums[l] < nums[minidx]) {
+            minidx = l;
+        }
+
+        return nums[minidx];
     }
 };
