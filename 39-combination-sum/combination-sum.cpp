7d076eef752d1b4fa1b5a141fe844a60dcd07f57
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-05-23 23:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (40.85%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // int sum(vector<int>&nums){
+    //     int n=nums.size();
+    //     int sum=0;
+    //     for(int i=0;i<n;i++){
+    //         sum+=nums[i];
+    //     }
+    //     return sum;
+    // }
+    vector<vector<int>>res;
+    void solve(int idx,int target,vector<int>&nums,vector<int>&curr){
+        if(target==0){
+            res.push_back(curr);
+            return ;
+        }
+        if(target>0){
+            curr.push_back(nums[idx]);
+            solve(idx,target-nums[idx],nums,curr);
+            curr.pop_back();
+        }
+       
+        if(idx+1<nums.size())
+        solve(idx+1,target,nums,curr);
+       
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>curr;
+        solve(0,target,candidates,curr);
+        return res;
+    }
+};
