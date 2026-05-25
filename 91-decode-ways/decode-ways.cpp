812141ab41c2740a9690cea7cbc81c5aa0399e9b
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-05-25 17:27:57 +0530

    Time: 2 ms (22.69%) | Memory: 8.4 MB (86.90%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int dp[101];
     int solve(int i,int n,string &s){
-        if(i==n){
-            return 1;
-        }
-        if(s[i]=='0')return 0;
-
         if(dp[i]!=-1) return dp[i];
-        
-        int take1char= solve(i+1,n,s);
 
+        if(i==n){
+            return dp[i]=1;
+        }
+        if(s[i]=='0')return dp[i]=0;
+
+        int take1char= solve(i+1,n,s);
         int take2char=0;
 
         if(i+1<n ){
@@ -18,6 +17,7 @@ public:
                 take2char=solve(i+2,n,s);
             }
         }
+        
         return dp[i]=take1char+take2char;
     }
     int numDecodings(string s) {
