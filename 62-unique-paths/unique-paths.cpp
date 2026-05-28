6a61eadc27e68e6377931593f0f7f046d2549a91
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-05-28 16:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.89%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp [101][101];
+
+    int solve(int i,int j,int m,int n){
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(i<0 || i>=m || j<0 ||j>=n){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int down=solve(i+1,j,m,n);
+        int right= solve(i,j+1,m,n);
+
+        return dp[i][j]=down + right;
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,m,n);
+    }
+};
