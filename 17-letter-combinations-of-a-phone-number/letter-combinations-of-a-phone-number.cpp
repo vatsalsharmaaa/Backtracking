979a3176246ef9665b901f8d0d24009872c2bd34
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-05-29 22:29:41 +0530

    Time: 2 ms (15.67%) | Memory: 9.4 MB (28.04%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,119 +1,26 @@
 class Solution {
-
-    vector<string> result;
-    vector<string > mp ={ "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 public:
+    vector<string> answer;
+    vector<string> mp ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void solve(string &digits, int idx , string &curr){
 
-    void solve(string &digits,int idx,string &curr){
-
-        if(idx==digits.length()){
-            result.push_back(curr);
+        if(digits.length()==idx){
+            answer.push_back(curr);
             return ;
-
         }
-
         int num= digits[idx]-'0';
         string alpha= mp[num];
 
-        for(char c: alpha){
-
+        for(char c:alpha){
             curr.push_back(c);
             solve(digits,idx+1,curr);
             curr.pop_back();
         }
+
     }
     vector<string> letterCombinations(string digits) {
-
         string curr="";
-
         solve(digits,0,curr);
-        return result;
-        
+        return answer;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     vector<string> result;
-//     vector<string> mp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-//     void solve(string &digits, int idx, string &curr) {
-//         if (idx == digits.length()) {
-//             result.push_back(curr);
-//             return;
-//         }
-//         int num = digits[idx] - '0';
-//         string alpha = mp[num];
-
-//         for (char c : alpha) {
-//             curr.push_back(c);
-//             solve(digits, idx + 1, curr);
-//             curr.pop_back();
-//         }
-//     }
-//     vector<string> letterCombinations(string digits) {
-//         if (digits.empty()) return {};
-//         string curr = "";
-//         solve(digits, 0, curr);
-//         return result;
-//     }
-// };
