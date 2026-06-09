6f526890258f78e9c1f9053f299ba67347409e7f
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-06-09 20:29:02 +0530

    Time: 86 ms (13.93%) | Memory: 78 MB (7.28%) - LeetSync

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,26 +1,72 @@
+// class Solution {
+// public:
+
+//     int dfs(int curr,int parent ,unordered_map<int ,vector<int>>&adj, vector<bool>& hasApple ){
+//         int time=0;
+//         for(auto neigh:adj[curr]){
+//             if(neigh==parent) continue;
+//             int totaltime=dfs(neigh,curr,adj,hasApple);
+//             if(totaltime>0 || hasApple[neigh]==true){
+//                 time+=totaltime+2;
+//             }
+
+//         }
+//         return time ;
+//     }
+//     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+//         unordered_map<int ,vector<int>>adj;
+//         for(auto e: edges){
+//             int u=e[0];
+//             int v=e[1];
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+//         return dfs(0,-1,adj,hasApple);
+//     }
+// };
+
 class Solution {
 public:
 
-    int dfs(int curr,int parent ,unordered_map<int ,vector<int>>&adj, vector<bool>& hasApple ){
-        int time=0;
-        for(auto neigh:adj[curr]){
-            if(neigh==parent) continue;
-            int totaltime=dfs(neigh,curr,adj,hasApple);
-            if(totaltime>0 || hasApple[neigh]==true){
-                time+=totaltime+2;
-            }
+    int dfs(int curr,int parent,unordered_map<int,vector<int>>&adj, vector<bool>& hasApple){
 
+        int time=0;
+        for(auto child: adj[curr]){
+            if(child==parent) continue;
+
+            int collect= dfs(child,curr,adj,hasApple);
+            if(collect>0 ||hasApple[child]){
+                time+=collect +2;
+            }
+            
         }
-        return time ;
+        return time;
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        unordered_map<int ,vector<int>>adj;
-        for(auto e: edges){
-            int u=e[0];
-            int v=e[1];
+        unordered_map<int,vector<int>>adj;
+        for(auto x: edges){
+            int u=x[0];
+            int v=x[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         return dfs(0,-1,adj,hasApple);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
