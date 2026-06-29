e8973b55791b3c235d5dd06298955680feb03f2b
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-06-29 17:13:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (59.90%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -13,9 +13,12 @@ class Solution {
 public:
 
     bool solve(TreeNode* n1 , TreeNode* n2){
+        
         if(n1==nullptr && n2==nullptr) return true;
         // if(!n1 && n2!=nullptr) return false;
         // if(n1!=nullptr && !n2) return false;
+
+        //agar dono null hote toh uppar se hojata return true
         if(!n1 || !n2) return false;
 
         if(n1->val != n2->val) return false;
