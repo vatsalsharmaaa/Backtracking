b70e7a614d771c8460f20cd1cf2ba73c08bbf928
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-07-09 23:31:23 +0530

    Time: 74 ms (31.85%) | Memory: 54.5 MB (21.63%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int n;
+    int solve(string s,int k){
+
+        if(s.length()==0) return 0;
+
+        vector<int> mp(26,0);
+        for(auto ch: s){
+            mp[ch-'a']++;
+        }
+      
+        for(int i=0;i<s.length();i++){
+            if(mp[s[i]-'a'] <k){
+                int left=solve(s.substr(0,i),k);
+                int right= solve(s.substr(i+1),k);
+
+                return max(left,right);
+            }
+        }
+            return s.size();
+
+    }
+    int longestSubstring(string s, int k) {
+        n=s.length();
+
+        return solve(s,k);
+    }
+};
