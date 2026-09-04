4008f46f034f7e02983cb4bb4ae94fcdb919aca8
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-09-05 01:40:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (79.57%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,31 +1,60 @@
+// class Solution {
+// public:
+//     typedef pair<int ,int>p;
+//     int lastStoneWeight(vector<int>& stones) {
+//         int n = stones.size();
+//         priority_queue<p,vector<p>>pq;
+//         for(int i=0;i<n;i++){
+//             pq.push({stones[i],i});
+//         }
+//         while(pq.size()>1){
+//             auto p1= pq.top();
+//             pq.pop();
+//             auto p2=pq.top();
+//             pq.pop();
+
+//             int y=p1.first;
+//             int x=p2.first;
+//             if(x==y){
+//                 continue;
+//             }
+//             else if(y>x){
+//                 y-=x;
+//                 pq.push({y,p1.second});
+//             }
+//         }
+//         if(pq.empty()){
+//             return 0;
+//         }
+//         else return pq.top().first;
+//     }
+// };
 class Solution {
 public:
-    typedef pair<int ,int>p;
     int lastStoneWeight(vector<int>& stones) {
-        int n = stones.size();
-        priority_queue<p,vector<p>>pq;
-        for(int i=0;i<n;i++){
-            pq.push({stones[i],i});
+
+        priority_queue<int> pq;
+
+        for(int stone : stones) {
+            pq.push(stone);
         }
-        while(pq.size()>1){
-            auto p1= pq.top();
-            pq.pop();
-            auto p2=pq.top();
+
+        while(pq.size() > 1) {
+
+            int y = pq.top();
             pq.pop();
 
-            int y=p1.first;
-            int x=p2.first;
-            if(x==y){
-                continue;
-            }
-            else if(y>x){
-                y-=x;
-                pq.push({y,p1.second});
+            int x = pq.top();
+            pq.pop();
+
+            if(y > x) {
+                pq.push(y - x);
             }
         }
-        if(pq.empty()){
+
+        if(pq.empty())
             return 0;
-        }
-        else return pq.top().first;
+
+        return pq.top();
     }
 };
