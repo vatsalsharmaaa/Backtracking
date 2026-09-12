500eee4aa35d7efd96abbbaed04f1ca1d51f88e3
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-09-12 23:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.43%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.cpp
+++ 1022-unique-paths-iii/unique-paths-iii.cpp
@@ -1,122 +1,77 @@
- class Solution {
- public:
-     vector<vector<int>>dirs={{0,-1},{-1,0},{0,1},{1,0}};
-     int zero=0;
-     int n,m;
-     int result;
-     int ti,tj;
+class Solution {
+public:
+    vector<vector<int>> dirs = {{0,-1},{-1,0},{0,1},{1,0}};
+    int zero = 0;
+    int n, m;
+    int result;
+    int ti, tj;
 
-     void solve(int i,int j,vector<vector<int>>& grid,int count){
-          if(i<0||j<0||i>=n||j>=m||grid[i][j]==-1) return ;
-         if(grid[i][j]==0)count++;
-        
-          if(i==ti &&j==tj ){
-                if(count==zero){
+    void solve(int i, int j, vector<vector<int>>& grid, int count) {
+
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == -1)
+            return;
+
+        if(grid[i][j] == 0)
+            count++;
+
+        // Reached destination
+        if(i == ti && j == tj) {
+            if(count == zero)
                 result++;
-                 return;
-                }
-         }
-       
-         int temp = grid[i][j];
-          grid[i][j] = -1;
-
-         for(auto dir:dirs){
-             int ni= i+ dir[0];
-             int nj= j+dir[1];
-            
-            solve(ni,nj,grid,count);
-         }
-         grid[i][j]=temp;
-     }
-     int uniquePathsIII(vector<vector<int>>& grid) {
-        n=grid.size();
-          m=grid[0].size();
-          int si=0,sj=0;
-          result=0;
-          int count=0;
-
-         for(int i=0;i<n;i++){
-             for(int j=0;j<m;j++){
-                 if(grid[i][j]==0) zero++; 
-                 if(grid[i][j]==1) {
-                     si=i;
-                     sj=j;
-                 }
-                 if(grid[i][j]==2){
-                     ti=i;
-                    tj=j;
-                }
-             }
-
+            return;
         }
-        solve(si,sj,grid,count);
-         return result;
+
+        int temp = grid[i][j];
+        grid[i][j] = -1;
+
+        for(auto &dir : dirs) {
+            int ni = i + dir[0];
+            int nj = j + dir[1];
+            solve(ni, nj, grid, count);
+        }
+
+        grid[i][j] = temp; // backtrack
     }
- };
 
-// class Solution {
-// public:
-//     vector<vector<int>> dirs = {{0,-1},{-1,0},{0,1},{1,0}};
-//     int zero = 0;
-//     int n, m;
-//     int result;
-//     int ti, tj;
+    int uniquePathsIII(vector<vector<int>>& grid) {
 
-//     void solve(int i, int j, vector<vector<int>>& grid, int count) {
+        n = grid.size();
+        m = grid[0].size();
 
-//         if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == -1)
-//             return;
+        int si = 0, sj = 0;
+        result = 0;
 
-//         if(grid[i][j] == 0)
-//             count++;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
 
-//         // Reached destination
-//         if(i == ti && j == tj) {
-//             if(count == zero)
-//                 result++;
-//             return;
-//         }
+                if(grid[i][j] == 0)
+                    zero++;
 
-//         int temp = grid[i][j];
-//         grid[i][j] = -1;
+                if(grid[i][j] == 1) {
+                    si = i;
+                    sj = j;
+                }
 
-//         for(auto &dir : dirs) {
-//             int ni = i + dir[0];
-//             int nj = j + dir[1];
-//             solve(ni, nj, grid, count);
-//         }
+                if(grid[i][j] == 2) {
+                    ti = i;
+                    tj = j;
+                }
+            }
+        }
 
-//         grid[i][j] = temp; // backtrack
-//     }
+        solve(si, sj, grid, 0);
 
-//     int uniquePathsIII(vector<vector<int>>& grid) {
+        return result;
+    }
+};
 
-//         n = grid.size();
-//         m = grid[0].size();
 
-//         int si = 0, sj = 0;
-//         result = 0;
 
-//         for(int i = 0; i < n; i++) {
-//             for(int j = 0; j < m; j++) {
 
-//                 if(grid[i][j] == 0)
-//                     zero++;
 
-//                 if(grid[i][j] == 1) {
-//                     si = i;
-//                     sj = j;
-//                 }
 
-//                 if(grid[i][j] == 2) {
-//                     ti = i;
-//                     tj = j;
-//                 }
-//             }
-//         }
 
-//         solve(si, sj, grid, 0);
 
-//         return result;
-//     }
-// };
+
+
+
