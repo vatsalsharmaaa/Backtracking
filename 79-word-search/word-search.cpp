2a1ddb78a7463ff56da941fbfb04829d9f1123d6
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-09-13 12:30:55 +0530

    Time: 632 ms (32.40%) | Memory: 122.8 MB (5.00%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,45 +1,57 @@
 class Solution {
 public:
-    int m, n;
-    vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
+    int n,m;
+    vector<vector<int>>dirs={{0,1},{1,0},{0,-1},{-1,0}};
+    int wl;
 
-    bool find(vector<vector<char>>& board, int i, int j, int idx, string &word) {
-
-        // Boundary check FIRST
-        if(i < 0 || j < 0 || i >= m || j >= n) return false;
-
-        // Already visited
-        if(board[i][j] == '$') return false;
-
-        // Character mismatch
-        if(board[i][j] != word[idx]) return false;
-
-        // Found all characters
-        if(idx == word.size() - 1) return true;
-
-        char temp = board[i][j];
-        board[i][j] = '$';  // mark visited
-
-        for(auto &dir : directions){
-            int ni = i + dir[0];
-            int nj = j + dir[1];
-
-            if(find(board, ni, nj, idx + 1, word))
-                return true;
+    bool solve(int idx,int i,int j,string &word,vector<vector<char>>& board)
+    {
+        if(idx==wl){
+            return true;
         }
+        
+        if(i<0||i>=n||j<0||j>=m||board[i][j]=='$'){
+            return false;
+        }
+        if(board[i][j]!=word[idx]) return false;
+        char temp=board[i][j];
+        board[i][j]='$';
 
-        board[i][j] = temp; // restore
+        for(auto dir:dirs){
+            int ni=i+dir[0];
+            int nj=j+dir[1];
+            if(solve(idx+1,ni,nj,word,board)){
+                return true;
+            }
+        }
+        board[i][j]=temp;
+
         return false;
+
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
+        n=board.size();
+        wl=word.size();
+        m=board[0].size();
+          vector<int> freq(128, 0);
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(board[i][j] == word[0] && find(board, i, j, 0, word))
-                    return true;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                freq[board[i][j]]++;
+            }
+        }
+         if(freq[word[0]] > freq[word[wl - 1]]) {
+            reverse(word.begin(), word.end());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0]){
+                    if(solve(0,i,j,word,board)){
+                        return true;
+                    }
+                }
             }
         }
         return false;
