c3c3fdbcfa0b6d4ab57004d3ba8075aee4379220
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-09-14 22:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (5.15%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,59 +1,75 @@
 class Solution {
 public:
-    vector<vector<int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    int n, m;
-    typedef pair<int, int> p;
-    int orangesRotting(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
+    vector<vector<int>>dirs={{0,1},{1,0},{-1,0},{0,-1}};
+    int n,m;
 
-        int fresh = 0;
-        queue<p> q;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        n=grid.size();
+        m=grid[0].size();
+        queue<pair<int,int>>q;
+
+        int fresh=0,time=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+
+                }
+                else if (grid[i][j]==1){
                     fresh++;
-                } else if (grid[i][j] == 2) {
-                    q.push({i, j});
                 }
             }
         }
-        if (fresh == 0) {
+        if(fresh==0){
             return 0;
         }
-        int time = 0;
-        while (!q.empty()) {
-            int size = q.size();
-            int temp = 0;
-            while (size--) {
-                int i = q.front().first;
-                int j = q.front().second;
+        while(!q.empty()){
+            int temp=0;
+            int size=q.size();
+
+            while(size--){
+                auto x= q.front();
                 q.pop();
+                int i=x.first;
+                int y=x.second;
 
-                for (auto dir : dirs) {
-
-                    int ni = i + dir[0];
-                    int nj = j + dir[1];
-                    if (ni >= 0 && ni < n && nj >= 0 && nj <m &&
-                        grid[ni][nj] == 1) {
+                for(auto dir:dirs){
+                    int ni=i+dir[0];
+                    int nj= y+dir[1];
+                    if(ni>=0&&ni<n&&nj>=0&&nj<m&&grid[ni][nj]==1){
                         temp++;
-                        grid[ni][nj] = 2;
-                        q.push({ni, nj});
+                        grid[ni][nj]=2;
+                        q.push({ni,nj});
                     }
                 }
-                
             }
-            if (temp > 0)
-                    time++;
+            if(temp>0){
+                time++;
+            }
+
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    time = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    time=0;
                     break;
                 }
             }
         }
-        return (time == 0) ? -1 : time;
+        return time==0?-1:time;
+
+
+
     }
 };
+
+
+
+
+
+
+
+
+
+
